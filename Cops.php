9c75bc508511d3b95254e578131cbf84937522eb
Darunia18 <darunia1818@gmail.com>
2014-06-05 19:19:18 -0400

    Update Cops.php
    Please learn how to code, learn the PocketMine API, and learn what labels mean before you just randomly start putting the word "cops" into everything.

--- Cops.php
+++ Cops.php
@@ -12,30 +12,35 @@ __PocketMine Plugin__
 class Cops implements Plugin
 {
     private $api;
+    private $config;
+    
     public function __construct(ServerAPI $api, $server = false)
     {
         $this->api = $api;
     }
+    
     public function init()
     {
+     $this->config = new Config($this -> path . "config.yml", CONFIG_YAML, array());
      $this->api->console->register("Cops","the main command", array($this, "command"));
-      $this->api->console->register("Cops add", "to add Cops", array($this, "command1"));
-	 $this->api->console->alias("Cops a","cops add")
-	 $this->cops = new Config($this -> path . "cops.yml", CONFIG_YAML, array());
+     //$this->api->console->register("Cops add", "to add Cops", array($this, "command1"));
+     //$this->api->console->alias("Cops a","cops add")
 
 
-
-		 public function command1($cmd, $args, $issuer)
+    public function command1($cmd, $args, $issuer)
     {
+        /*The whole command needs to be reworked completely
         switch($args[0])
         {
             case "Cop" :
+            //Random case with another case inside, that makes no sense?
             case "cops" :
                 $target = $args[1];
                 array_push($this -> cops, $target);
                 $this -> cops = $this -> api -> plugin -> writeYAML($this -> path . "cops.yml", $target);
                 return "the cop " . $target . " is ready for service";
                 break;
+		//Else statement for no reason?
 		else{
 		 $output="ERORR"
 		 return $output;
@@ -52,11 +57,11 @@ class Cops implements Plugin
         
       {
       	
-      	 $this->api->console->run("give " . $target . "345 1" . 
-      	 $this->api->console->run("give " . $target . "288 1 " . 
-         $this->api->console->run("give " . $target . "364 64" . 
-      	 $this->api->console->run("give " . $target . "50 64" . 
-      	 $this->api->console->run("give " . $target . "267 1" . 
+      	 $this->api->console->run("give " . $target . " 345 1");
+      	 $this->api->console->run("give " . $target . " 288 1");
+         $this->api->console->run("give " . $target . " 364 64");
+      	 $this->api->console->run("give " . $target . " 50 64");
+      	 $this->api->console->run("give " . $target . " 267 1");
       	
       }
       	
@@ -70,9 +75,9 @@ class Cops implements Plugin
                 $this -> api -> plugin -> writeYAML($this -> path . "cops.yml", $this -> config);
                 
       	}
-      	 $this -> api -> chat -> sendTo(false,"[Cops] you are now a cop"
+      	 $this -> api -> chat -> sendTo(false,"[Cops] you are now a cop");
       
-      }
+      }*/
 }
 
  public function __destruct()
